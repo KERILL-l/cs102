f7d11e968a37d5611e5eb7da0ca54b452698edb8
KERILL-l <366726@niuitmo.ru>
2023-10-25 20:19:46 +0300

    lab0,1

--- tests/lab1/test_calculator.py
+++ tests/lab1/test_calculator.py
@@ -1,7 +1,20 @@
 import unittest
+import sys
+sys.path.insert(0, '/home/kirill/cs102/src')
+from lab1.calculator import calculate
 
-class CalculatorTestCase(unittest.TestCase):
+class Test(unittest.TestCase):
+        def test_one(self):
+                self.assertEqual(calculate(1,2, '+'), 3)
+                self.assertEqual(calculate(1,2, '-'), -1)
+                self.assertEqual(calculate(1,2, '*'), 2)
+                self.assertEqual(calculate(1,2, '/'), 0.5)
+                self.assertEqual(calculate(1,2, 'Сори за пропущенный дедлайн:(((((('), 'ERROR')
+                self.assertEqual(calculate('Еще раз сори',2, '+'), 'ERROR')
+                self.assertEqual(calculate(1,'И закрепительный сорян', '+'), 'ERROR')
 
-    # Тест для проверки работы, можно удалить
-    def test_one(self):
-        self.assertEquals(1, 1)
+                
+
+
+if __name__ == "__main__":
+         unittest.main()
